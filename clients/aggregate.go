7d4cb746a098c73993212bf20707a4c484b891f7
Rayquen <rayquen.zero@gmail.com>
2021-07-12 23:16:59 -0300

    agregado de cliente

---
+++ clients/aggregate.go
@@ -0,0 +1,50 @@
+package clients
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Client struct {
+	ID            int64  `json:"id"`
+	Name          string `json:"name"`
+	Address       string `json:"address"`
+	Telephone     string `json:"telephone"`
+	Whatsapp      string `json:"whatsapp"`
+	Facebook      string `json:"facebook"`
+	DeliveryTimes string `json:"deliveryTimes"`
+	Comments      string `json:"comments"`
+}
+
+type IClient interface {
+}
+
+func NewClient(id int64,
+	name string,
+	address string,
+	telephone string,
+	whatsapp string,
+	facebook string,
+	deliveryTimes string,
+	comments string) (*Client, error) {
+	c := new(Client)
+
+	if id <= 0 {
+		return nil, fmt.Errorf("el ID debe ser un número positivo: %v", id)
+	}
+
+	if strings.Trim(name, "\t \n") == "" {
+		return nil, fmt.Errorf("el cliente debe tener un nombre válido: %v", name)
+	}
+
+	c.ID = id
+	c.Name = name
+	c.Address = address
+	c.Telephone = telephone
+	c.Whatsapp = whatsapp
+	c.Facebook = facebook
+	c.DeliveryTimes = deliveryTimes
+	c.Comments = comments
+
+	return c, nil
+}
